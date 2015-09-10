3e81069e22035866baa7b79d883e873f6eca067f
Jared Sprague <jared@soulfiregems.com>
2015-09-09 23:49:57 -0400

    Added ability to GET and POST highscores

--- index.php
+++ index.php
@@ -5,7 +5,30 @@ require_once '/var/www/html/fishapi/Slim/Slim.php';
 $app = new \Slim\Slim();
 
 $app->get('/highscores/', function () {
-    echo "TODO return high scors as JSON";
+    // Add the score to the current high scores list
+    try {
+        $highscores_json = file_get_contents('highscores.json');
+    } catch (exception $e) {
+        error_log("highscores.json not found");
+        $highscores_json = '[]';
+    }
+
+    echo $highscores_json;
+});
+
+$app->post('/highscores/', function () use ($app) {
+    try {
+        $highscores_array = json_decode(file_get_contents('highscores.json'), true);
+    } catch (exception $e) {
+        error_log("highscores.json not found");
+        $highscores_array = array();
+    }
+
+    $highscores_array[] = $_POST;
+    rsort($highscores_array);
+
+    // sort and write out the file
+    file_put_contents('highscores.json', json_encode($highscores_array));
 });
 
 $app->run();
